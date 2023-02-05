d7fecd81016bc084ac63f9ceb0124bfa526ac40a
Allen R. Marshall <armarshall@allenrmarshall-consulting-llc.com>
2023-02-05 09:43:28 -0500

    Added comments

--- files/data/scripts/Script_Test_SQL_FILTER_Values_For_Required_Colums.sql
+++ files/data/scripts/Script_Test_SQL_FILTER_Values_For_Required_Colums.sql
@@ -1,3 +1,25 @@
+/* ARMarshall, ARM LLC - 20230205 - 
+
+a script to  check every value of SQL _FILTER in PON_FILTERS with a regular expression that confirms basic syntax correctness
+usage:  Execute this SQL in a SQL window while connected to a BLP database
+Any records that are returned with a 1 for compliant confom to the basic requirement:
+a) must include SELECT b.bridge_gd as key_
+b) must include .... b.brkey somewhere
+c) must use the alias b. for bridge
+
+Caveats:
+Does not actually test the syntax for correctness
+Case-sensitivity is ignored.
+
+Other:
+
+Use  the script Script_Check_Desktop_SQL.pdc to exercise every filter and show the # of records expected (no user access filter applied)
+Any records returning 0 rows may have SQL errors.
+
+
+
+*/
+
 select pf.filterkey,
        pf.name,
        nvl(1, 0) as compliant,
